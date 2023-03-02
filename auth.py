67a307700222165bcd17daed8f63da51879269fc
karan-2004 <kara80499@gmail.com>
2023-03-02 18:26:01 +0530

    pc app

--- auth.py
+++ auth.py
@@ -1,27 +1,23 @@
 import database as db
-import main
 class password:
-    def __init__(self) -> None:
-        self.stat=[0,0]
-    def auth(self):
-        pas=input("Enter your password:")
+    def auth(self,x):
+        self.stat=[1,1]
+        a=[str(x)]
         crsr=db.data.db.cursor()
-        #crsr.execute("SELECT emp_id FROM employees WHERE passcode=%s",tuple(pas))
-        #for i in crsr:
-        #   print(i)
-        #    emp=i
-        if(self.stat[emp-1]==0):
-            crsr.execute("UPDATE REGISTER SET ENTRY_TIME=NOW() WHERE EMP_ID=2 AND DATE_OF_REGISTER=CURRENT_DATE AND CR_STATUS='O'")
+        crsr.execute("SELECT emp_id FROM employees WHERE passcode=%s",tuple(a))
+        for i in crsr:
+           emp=i  
+        if(self.stat[emp[0]-1]==0):
+            crsr.execute("UPDATE REGISTER SET ENTRY_TIME=NOW() WHERE EMP_ID=%s AND DATE_OF_REGISTER=CURRENT_DATE AND CR_STATUS='O'",emp)
             #
             db.data.db.commit()
-            self.stat[emp-1]=1
+            self.stat[emp[0]-1]=1
             main.main()
         else:
-            crsr.execute("UPDATE REGISTER SET EXIT_TIME=NOW() WHERE EMP_ID=2 AND DATE_OF_REGISTER=CURRENT_DATE AND CR_STATUS='I'")
+            crsr.execute("UPDATE REGISTER SET EXIT_TIME=NOW() WHERE EMP_ID=%s AND DATE_OF_REGISTER=CURRENT_DATE AND CR_STATUS='O'",emp)
             #
             db.data.db.commit()    
-            self.stat[emp-1]=0
-            main.main()
-authen=password()           
+            self.stat[emp[0]-1]=0
+           
 
     
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import auth 
+from auth import password 
 import employee as em
 #em.emp.show()
-def main():
-    auth.authen.auth()
+def main(): 
+    while True:
+        pas=int(input('Enter your password:'))
+        if(pas>9998):
+            break
+        password.auth(password,pas)
 main()    
     
